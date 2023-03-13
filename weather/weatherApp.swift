55431c682ceb20a12eef85489d105bb8c163d1f6
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-13 21:15:09 +0900

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by juno on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
